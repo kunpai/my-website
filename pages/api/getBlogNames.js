892b9ef5e1180c0b84d6118ed59c332d5472da2e
kunpai <kunpai@ucdavis.edu>
2023-07-04 15:44:47 -0700

    Search bar for blogs
    Co-authored-by: Parth Shah <helloparthshah@gmail.com>

--- pages/api/getBlogNames.js
+++ pages/api/getBlogNames.js
@@ -3,6 +3,8 @@ import parseMD from 'parse-md'
 import fs from 'fs';
 
 export default function handler(req, res) {
+    const query = req.body.query ?? '';
+    const keywords = query.split(' ');
     let names = []
     const blogDirectory = path.resolve('./public', 'blogs');
     const blogNames = fs.readdirSync(blogDirectory);
@@ -10,6 +12,7 @@ export default function handler(req, res) {
     blogNames.forEach((name, index) => {
         const blogContent = fs.readFileSync(path.join(blogDirectory, name), 'utf8');
         const { metadata, content } = parseMD(blogContent);
+
         names.push({
             name: name.slice(0, -3),
             title: metadata.title,
@@ -20,9 +23,81 @@ export default function handler(req, res) {
             content: content
         });
     });
+
+    names = names.filter((name, index) => {
+        if (query === '') {
+            return true;
+        }
+        for (let tag of name.tags) {
+            if (tag.toLowerCase().includes(query.toLowerCase())) {
+                return true;
+            }
+        }
+        for (let author of name.authors) {
+            if (author.toLowerCase().includes(query.toLowerCase())) {
+                return true;
+            }
+        }
+        let authorDistances = keywords.map((keyword, index) => {
+            // split the author name into first and last name
+            let authorNames = name.authors.map((author) => author.toLowerCase().split(' '));
+            // flatten the array
+            authorNames = [].concat.apply([], authorNames);
+            // return the minimum distance between the keyword and the author's first and last name
+            return Math.min(...authorNames.map((author) => damerauLevenshteinDistance(keyword, author.toLowerCase())));
+        });
+        if (Math.min(...authorDistances) < 2) {
+            return true;
+        }
+        if (name.title.toLowerCase().includes(query.toLowerCase())) {
+            return true;
+        }
+        let titleDistances = keywords.map((keyword, index) => {
+            return Math.min(...name.title.toLowerCase().split(' ').map((word) => damerauLevenshteinDistance(keyword, word)));
+        });
+        return Math.min(...titleDistances) < 2;
+    });
+
     // sort by date
     names.sort((a, b) => {
         return new Date(b.date) - new Date(a.date);
     });
     res.status(200).json(names);
+}
+
+function damerauLevenshteinDistance(a, b) {
+    if (a.length == 0) return b.length;
+    if (b.length == 0) return a.length;
+    var matrix = [];
+    for (var i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+    for (var j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j - 1] + 1, // substitution
+                    matrix[i][j - 1] + 1, // insertion
+                    matrix[i - 1][j] + 1 // deletion
+                );
+                if (
+                    i > 1 &&
+                    j > 1 &&
+                    b.charAt(i - 1) == a.charAt(j - 2) &&
+                    b.charAt(i - 2) == a.charAt(j - 1)
+                ) {
+                    matrix[i][j] = Math.min(
+                        matrix[i][j],
+                        matrix[i - 2][j - 2] + 1 // transposition
+                    );
+                }
+            }
+        }
+    }
+    return matrix[b.length][a.length];
 }
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,32 +1,78 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 import BlogTile from '@/components/blogTile';
+import Image from 'next/image';
+import searchImage from '@/public/images/search.png'
+import { useRouter } from "next/router";
 
 export default function Blogs() {
     const [blogs, setBlogs] = useState([]);
+    const router = useRouter();
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
-        fetch('/api/getBlogNames').then(res => res.json()).then(data => {
-            setBlogs(data);
-        }
-        );
-    }, []);
+        // get query from url ?query=
+        const query = router.query.query;
+        setQuery(query);
+    }, [router.query.query]);
+
+    useEffect(() => {
+        fetch('/api/getBlogNames', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'query': query,
+            }),
+        })
+            .then(res => res.json()).then(data => {
+                setBlogs(data);
+            });
+    }, [query]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const search = data.get('search');
+        router.push({
+            pathname: '/blogs',
+            query: { query: search },
+            shallow: true,
+        });
+    }
     return (
         <Container className="blogs">
             <h1 className="text-center mt-5" style={{ fontSize: '3rem' }}>
                 Blogs
             </h1>
+            <Form className={"search-form w-100"} onSubmit={handleSubmit}>
+                <Form.Control type="search" name="search" placeholder="Search Blogs" className="main-text-regular" />
+                <div id="search-icon" className="d-flex align-items-center justify-content-center" onClick={() => document.getElementById("submit").click()}>
+                    <Image
+                        src={searchImage}
+                        alt="Search Icon"
+                        height="20"
+                        type="submit"
+                    />
+                </div>
+                <Button type="submit" style={{ display: "none" }} id="submit" role="button" >
+                    Search
+                </Button>
+            </Form>
             <hr />
-            {blogs.map(blog => {
-                return (
-                    <>
-                        <BlogTile blog={blog} />
-                        <hr />
-                    </>
-                );
+            {
+                blogs.length === 0 ? <h2 className="text-center mt-5">No Blogs Found</h2> :
+                    blogs.map(blog => {
+                        return (
+                            <>
+                                <BlogTile blog={blog} />
+                                <hr />
+                            </>
+                        );
+                    })
             }
-            )}
         </Container>
     );
 }
