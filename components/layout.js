a92d13bdbe535754690ac4f5d3ad9334df88f1ac
kunpai <kunpai@ucdavis.edu>
2023-07-09 10:35:37 -0700

    dark mode layout

--- components/layout.js
+++ components/layout.js
@@ -2,13 +2,21 @@ import Topbar from './topbar'
 import { Fade } from 'react-bootstrap'
 import Footer from './footer'
 import ScrollToTop from './scrollToTop'
+import { useEffect, useState } from 'react';
 
 export default function Layout({ children }) {
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+      function returnDarkMode() {
+        return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+      }
+      setDarkMode(returnDarkMode());
+    }, []);
     return (
         <>
             <Topbar />
             <Fade in={true} appear={true} timeout={10}>
-                <main style={{ minHeight: 'calc(100vh - 100px)' }} aria-label='main'>
+                <main style={{ minHeight: 'calc(100vh - 100px)', background: darkMode ? "black" : "white"  }} aria-label='main'>
                     {children}
                 </main>
             </Fade>
