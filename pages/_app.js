c5e6d9e797a6f7386ebe840fb98b5bfba2ae4310
kunpai <kunpai@ucdavis.edu>
2023-07-10 19:22:20 -0700

    Configuration Co-authored-by: Parth Shah <helloparthshah@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Kunal Pai</title>
+        <title>{process.env.CONFIG.name}</title>
         <meta name="description" content="Kunal Pai's personal website." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
