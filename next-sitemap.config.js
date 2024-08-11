78b678e245401e8835a706113e6bf6e92e50198d
kunpai <kunpai@ucdavis.edu>
2024-08-11 15:15:44 -0700

    feat: Add next-sitemap configuration file and update npm dependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.kunpai.space/',
+    generateRobotsTxt: true,
+    // Add any additional configuration options here
+  };
