0a75461565da95788d3d2bde8212c927b501a8ab
Kunal Pai <kunpai@ucdavis.edu>
2026-05-28 18:32:52 -0700

    fix: add close button to kunalai chat

--- components/kunalai.js
+++ components/kunalai.js
@@ -222,7 +222,16 @@ ${serviceList}
                 aria-label="Toggle KunalAI Chatbot"
             >
                 {isOpen ? (
-                    <i className="bi bi-x-lg"></i>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        className="bi bi-x-lg"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                    </svg>
                 ) : (
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -253,11 +262,21 @@ ${serviceList}
                         </div>
                         <Button
                             variant="link"
-                            className="p-0 text-muted"
+                            className="p-0 text-muted d-flex align-items-center justify-content-center"
                             onClick={() => setIsOpen(false)}
                             aria-label="Close chat"
+                            style={{ width: "24px", height: "24px", minWidth: "24px" }}
                         >
-                            <i className="bi bi-x-lg"></i>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                fill="currentColor"
+                                className="bi bi-x-lg"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                            </svg>
                         </Button>
                     </Card.Header>
 
